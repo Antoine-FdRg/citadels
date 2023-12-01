74fa83a860a7efa95fb8361503f5317c9f972c75
BaptisteLacroix <baptiste.lacroix03@gmail.com>
2023-12-01 15:54:20 +0100

    feat(intial-commit): initil commit with maven and github action for pipeline

--- src/main/java/com/seinksansdoozebank/fr/Main.java
+++ src/main/java/com/seinksansdoozebank/fr/Main.java
@@ -1,4 +1,4 @@
-package fr.cotedazur.univ.polytech.startingpoint;
+package com.seinksansdoozebank.fr;
 
 public class Main {
 
--- src/test/java/com/seinksansdoozebank/fr/MainTest.java
+++ src/test/java/com/seinksansdoozebank/fr/MainTest.java
@@ -1,4 +1,4 @@
-package fr.cotedazur.univ.polytech.startingpoint;
+package com.seinksansdoozebank.fr;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
