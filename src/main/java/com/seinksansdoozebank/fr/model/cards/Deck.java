622ec24efbc759decdf28d5ab6c5ab1d69ceefd8
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2023-12-05 08:51:33 +0100

    feat(Deck): added the class Deck, implemented the getters and added tests #1

--- src/main/java/com/seinksansdoozebank/fr/model/cards/Deck.java
+++ src/main/java/com/seinksansdoozebank/fr/model/cards/Deck.java
@@ -1,7 +1,46 @@
 package com.seinksansdoozebank.fr.model.cards;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
-    List<District> deck;
+    private List<District> deck;
+    private static final int NUMBER_OF_CARDS = 65;
+
+
+    /**
+     * Constructor which implements a new deck of 65 districts
+     */
+    public Deck() {
+        this.deck = new ArrayList<>();
+        Random random = new Random();
+        int cost;
+        for (int i = 0; i < NUMBER_OF_CARDS; i++) {
+            cost = 1 + random.nextInt(5 - 1);
+            this.deck.add(new District(cost));
+        }
+    }
+
+
+    /**
+     * @param index
+     * @return the district which is at the specified index in the deck
+     * @throws ArrayIndexOutOfBoundsException
+     */
+    public District getDistrictWithIndex(int index) throws ArrayIndexOutOfBoundsException {
+        if (index > deck.size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return deck.get(index);
+    }
+
+    /**
+     * getter
+     * @return the deck of 65 districts
+     */
+    public List<District> getDeck() {
+        return deck;
+    }
+
 }
