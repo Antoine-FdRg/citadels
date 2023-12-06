f6622a5d4ca03ba9d34ead241efe9af0e280993d
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2023-12-06 10:40:52 +0100

    feat(DeckTest): deleted the method getDistrictWithIndex added the method pick and shuffle and tests  #1

--- src/main/java/com/seinksansdoozebank/fr/model/cards/Deck.java
+++ src/main/java/com/seinksansdoozebank/fr/model/cards/Deck.java
@@ -1,6 +1,7 @@
 package com.seinksansdoozebank.fr.model.cards;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -16,25 +17,50 @@ public class Deck {
         this.districtList = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < NUMBER_OF_CARDS; i++) {
-            this.districtList.add(new District(random.nextInt(1,5)));
+            this.districtList.add(new District(random.nextInt(1, 5)));
         }
+        shuffle();
     }
 
+    /**
+     * @return the last element of districtList
+     */
+    public District pick() {
+        //On vérifie que la liste n'est pas vide
+        if (districtList.isEmpty()) {
+            //TODO milestone 2 remettre la fausse dans la liste de District
+            //On recrée le deck
+            Random random = new Random();
+            for (int i = 0; i < NUMBER_OF_CARDS; i++) {
+                this.districtList.add(new District(random.nextInt(1, 5)));
+            }
+            shuffle();
+        }
+        //On récupère le dernier district de la liste
+        District districtToBePicked = districtList.get(districtList.size() - 1);
+        //On enlève à la liste le district pioché
+        districtList.remove(districtList.size() - 1);
+        //On renvoie le district pioché
+        return districtToBePicked;
+    }
 
     /**
-     * @param index
-     * @return the district which is at the specified index in the deck
-     * @throws ArrayIndexOutOfBoundsException
+     * The method shuffle takes the list of districts and shuffles it
      */
-    public District getDistrictWithIndex(int index) throws ArrayIndexOutOfBoundsException {
-        if (index > districtList.size()) {
-            throw new ArrayIndexOutOfBoundsException();
+    public void shuffle() {
+        Random random = new Random();
+        //On commence par la dernière carte du paquet
+        for (int i = districtList.size() - 1; i >= 1; i--) {
+            //on choisit un index au hasard parmi les autres éléments
+            int j = random.nextInt(i + 1);
+            //On échange l'élément à la i-ème place avec celui à la j-ème place
+            Collections.swap(districtList, i, j);
         }
-        return districtList.get(index);
     }
 
     /**
      * getter
+     *
      * @return the deck of 65 districts
      */
     public List<District> getDeck() {
--- src/test/java/com/seinksansdoozebank/fr/model/cards/DeckTest.java
+++ src/test/java/com/seinksansdoozebank/fr/model/cards/DeckTest.java
@@ -3,35 +3,64 @@ package com.seinksansdoozebank.fr.model.cards;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DeckTest {
 
     private Deck districtList;
+    private Deck districtListTest;
+
     @BeforeEach
-    void setup(){
-        districtList= new Deck();
+    void setup() {
+        districtList = new Deck();
+        districtListTest = new Deck();
     }
 
     /**
      * On vérifie que le deck possède bien 65 cartes quartiers lors de sa création
      */
     @Test
-    void deckCreationTest(){
-        assertEquals(65,districtList.getDeck().size());
+    void deckCreationTest() {
+        assertEquals(65, districtList.getDeck().size());
     }
 
     /**
      * On vérifie que le coût attribué à chaque quartier est compris entre 1 et 5
      */
     @Test
-    void getDistrictCostWIthDeckTest(){
-        for(int i=0;i<65;i++){
-            assertTrue(districtList.getDistrictWithIndex(i).getCost()>=1);
-            assertTrue(districtList.getDistrictWithIndex(i).getCost()<=5);
+    void getDistrictCostWIthDeckTest() {
+        for (int i = 0; i < 65; i++) {
+            assertTrue(districtList.getDeck().get(i).getCost() >= 1);
+            assertTrue(districtList.getDeck().get(i).getCost() <= 5);
         }
+    }
+
+    /**
+     * We verify when the list of districts is shuffled that it has differences with the one which is not shuffle.
+     */
+    @Test
+    void shuffleTest() {
+        districtListTest.shuffle();
+        int differencesCount = 0;
+        for (int i = 0; i < 65; i++) {
+            if (districtListTest.getDeck().get(i).compareTo(districtList.getDeck().get(i)) != 0) {
+                differencesCount++;
+            }
+        }
+        assertTrue(differencesCount > 0);
+    }
+
+    /**
+     * We verify that if the list is empty, the deck is built again
+     * This method will be improved with the milestone 2
+     */
+    @Test
+    void pickDistrictTest() {
+        for (int i = 0; i < 65; i++) {
+            districtList.pick();
+        }
+        districtList.pick();
+        assertEquals(64, districtList.getDeck().size());
     }
 }
