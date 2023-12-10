fb8f3be28a3deafa4bd58c6a7b320ea70a5f3951
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2023-12-10 15:09:47 +0100

    feat(roles): adding the enum roles and adding the commons roles. #19

--- src/main/java/com/seinksansdoozebank/fr/model/character/interfaces/Character.java
+++ src/main/java/com/seinksansdoozebank/fr/model/character/interfaces/Character.java
@@ -1,10 +1,14 @@
-package com.seinksansdoozebank.fr.model.player;
+package com.seinksansdoozebank.fr.model.character;
 
 
 import com.seinksansdoozebank.fr.model.cards.DistrictType;
+import com.seinksansdoozebank.fr.model.character.roles.Role;
 
 public abstract class Character {
-    private String name;
+    private Role role;
+
+    public Character(Role role) {
+    }
 
     public abstract void useEffect();
 
---
+++ src/main/java/com/seinksansdoozebank/fr/model/character/roles/Role.java
@@ -0,0 +1,13 @@
+package com.seinksansdoozebank.fr.model.character.roles;
+
+public enum Role {
+    MERCHANT,
+    KING,
+    BISHOP,
+    CONDOTTIERE,
+
+    //ARCHITECT("Architecte"),
+    //MAGICIAN("Magicien"),
+    //THIEF("Voleur"),
+    //ASSASSIN("Assassin");
+}
