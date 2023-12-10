465131dc73e82cf59c60407ce2e6f8cb682b51c8
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2023-12-10 16:24:08 +0100

    feat(Bishop): adding the Bishop character with the possibility to gain 1 gold per district. #19

---
+++ src/main/java/com/seinksansdoozebank/fr/model/character/singleton/Bishop.java
@@ -0,0 +1,52 @@
+package com.seinksansdoozebank.fr.model.character.singleton;
+
+import com.seinksansdoozebank.fr.model.cards.District;
+import com.seinksansdoozebank.fr.model.cards.DistrictType;
+import com.seinksansdoozebank.fr.model.character.abstracts.CommonCharacter;
+import com.seinksansdoozebank.fr.model.character.roles.Role;
+import com.seinksansdoozebank.fr.model.player.Player;
+
+import java.util.List;
+
+public class Bishop extends CommonCharacter {
+    private final Player player;
+    private final List<District> citadel;
+    private static final Role role = Role.BISHOP;
+    private static final DistrictType target = DistrictType.RELIGION;
+
+
+    public Bishop(List<District> citadel, Player player) {
+        super(role);
+        this.citadel = citadel;
+        this.player = player;
+    }
+
+    @Override
+    public void useEffect() {
+        // Nothing to do
+    }
+
+    /**
+     * For each district in the citadel of the target type, the player will collect one gold
+     */
+    @Override
+    public void goldCollectedFromDisctrictType() {
+        int nbGold = 0;
+        for (District district : this.citadel) {
+            if (district.getDistrictType() == target) {
+                nbGold++;
+            }
+        }
+        this.player.increaseGold(nbGold);
+    }
+
+    /**
+     * Method called to perform the action of the character
+     * This method will ask the user to choose an action to perform
+     * The action will be performed by the character
+     */
+    @Override
+    public void performAction() {
+        this.goldCollectedFromDisctrictType();
+    }
+}
