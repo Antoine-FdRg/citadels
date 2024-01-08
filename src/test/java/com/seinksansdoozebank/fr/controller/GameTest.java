ee1f2c2e2c8ae76ccc7db2ac18f9e10da00e00cd
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-01-08 19:31:50 +0100

    fix(gameTest): remove unused import #109

--- src/test/java/com/seinksansdoozebank/fr/controller/GameTest.java
+++ src/test/java/com/seinksansdoozebank/fr/controller/GameTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
