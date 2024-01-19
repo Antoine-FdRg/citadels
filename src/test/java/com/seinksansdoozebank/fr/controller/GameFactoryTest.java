fc469f748c449c23a5162bb3e0470d352bdc1bd6
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-01-19 14:06:56 +0100

    test(gameFactory): test all numberof player in Gamefactory.createGameOfRandomBot #130

--- src/test/java/com/seinksansdoozebank/fr/controller/GameFactoryTest.java
+++ src/test/java/com/seinksansdoozebank/fr/controller/GameFactoryTest.java
@@ -20,13 +20,23 @@ class GameFactoryTest {
     }
 
     @Test
-    void createGameOfRandomBotWithMoreThan6BotThrowException() {
-        assertThrows(IllegalArgumentException.class, () -> GameFactory.createGameOfRandomBot(view, 7));
-    }
-
-    @Test
     void createGameOfRandomBotWith4Bot() {
         Game game = GameFactory.createGameOfRandomBot(view, 4);
         assertEquals(4, game.players.size());
+    }
+    @Test
+    void createGameOfRandomBotWith5Bot() {
+        Game game = GameFactory.createGameOfRandomBot(view, 5);
+        assertEquals(5, game.players.size());
+    }
+    @Test
+    void createGameOfRandomBotWith6Bot() {
+        Game game = GameFactory.createGameOfRandomBot(view, 6);
+        assertEquals(6, game.players.size());
+    }
+
+    @Test
+    void createGameOfRandomBotWithMoreThan6BotThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> GameFactory.createGameOfRandomBot(view, 7));
     }
 }
