93ecb2bae68d5719c45db77834183ac724b4ec83
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-01-25 21:54:46 +0100

    fix(logs): fix lab logs #189

--- src/main/java/com/seinksansdoozebank/fr/view/Cli.java
+++ src/main/java/com/seinksansdoozebank/fr/view/Cli.java
@@ -176,12 +176,12 @@ public class Cli implements IView {
 
     @Override
     public void displayPlayerDiscardCard(Player player, Card card) {
-        CustomLogger.log(Level.INFO, "{0} défausse la carte {1}.", new Object[]{player, card});
+        CustomLogger.log(Level.INFO, "{0} défausse {1}.", new Object[]{player, card});
     }
 
     @Override
     public void displayPlayerUseLaboratoryEffect(Player player) {
-        CustomLogger.log(Level.INFO, "{0} utilise le laboratoire pour défausser une carte et gagner une pièce d'or.", player);
+        CustomLogger.log(Level.INFO, "{0} utilise le laboratoire pour défausser une carte et gagner une pièce d''or (s''il en reste dans la banque).", player);
     }
 
     @Override
