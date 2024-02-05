b5fdd866c62b742ec8ad2a43cf129f76995b5e98
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-02-05 13:15:05 +0100

    refactor: moving CommandLineArgs class. #220

--- src/main/java/com/seinksansdoozebank/fr/Launcher.java
+++ src/main/java/com/seinksansdoozebank/fr/Launcher.java
@@ -1,7 +1,6 @@
 package com.seinksansdoozebank.fr;
 
 import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 import com.seinksansdoozebank.fr.controller.Game;
 import com.seinksansdoozebank.fr.controller.GameBuilder;
 import com.seinksansdoozebank.fr.model.cards.Deck;
@@ -34,30 +33,5 @@ public class Launcher {
                         new CardChoosingStrategy())
                 .build();
         game.run();
-    }
-
-    // Class to hold command-line parameters
-    private static class CommandLineArgs {
-        @Parameter(names = "--2thousands", description = "Enable 2thousands option")
-        private boolean is2Thousands;
-
-        @Parameter(names = "--demo", description = "Enable demo option")
-        private boolean isDemo;
-
-        @Parameter(names = "--csv", description = "Enable CSV option")
-        private boolean isCsv;
-
-        // Getter methods if needed
-        public boolean is2Thousands() {
-            return is2Thousands;
-        }
-
-        public boolean isDemo() {
-            return isDemo;
-        }
-
-        public boolean isCsv() {
-            return isCsv;
-        }
     }
 }
