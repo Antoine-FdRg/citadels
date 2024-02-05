9f2e574b554d1bf43df8eb5eb45cbf6668b817a6
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-02-05 22:36:12 +0100

    feat(chooseCharacter): added chooseCharacterImpl method #227

--- src/main/java/com/seinksansdoozebank/fr/model/player/RichardBot.java
+++ src/main/java/com/seinksansdoozebank/fr/model/player/RichardBot.java
@@ -1,11 +1,68 @@
 package com.seinksansdoozebank.fr.model.player;
 
 import com.seinksansdoozebank.fr.model.cards.Deck;
+import com.seinksansdoozebank.fr.model.character.abstracts.Character;
+import com.seinksansdoozebank.fr.model.character.roles.Role;
 import com.seinksansdoozebank.fr.view.IView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class RichardBot extends SmartBot {
 
     public RichardBot(int nbGold, Deck deck, IView view) {
         super(nbGold, deck, view);
     }
+
+    @Override
+    public Character chooseCharacterImpl(List<Character> characters) {
+        List<Character> orderedCharacters = ordinateCharacters(characters);
+        Optional<Character> optionalCharacter = shouldChooseBecauseLastCardToBuy(characters);
+        if (optionalCharacter.isPresent()) {
+            return optionalCharacter.get();
+        }
+        for (Character character : orderedCharacters) {
+            switch (character.getRole()) {
+                case ASSASSIN -> {
+                    if (shouldChooseAssassin()) {
+                        return character;
+                    }
+                }
+                case MAGICIAN -> {
+                    if (shouldChooseMagician()) {
+                        return character;
+                    }
+                }
+                case MERCHANT -> {
+                    if (shouldChooseMerchant()) {
+                        return character;
+                    }
+                }
+                case ARCHITECT -> {
+                    if (shouldChooseArchitect()) {
+                        return character;
+                    }
+                }
+                case BISHOP -> {
+                    if (shouldChooseBishop()) {
+                        return character;
+                    }
+                }
+                case CONDOTTIERE -> {
+                    if (shouldChooseCondottiere()) {
+                        return character;
+                    }
+                }
+                default -> {
+                    break;
+                }
+            }
+        }
+        return super.chooseCharacterImpl(characters);
+    }
+
+
+
+
 }
