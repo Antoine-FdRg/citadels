fdc490f7220947beb8c209361033da6768f74da3
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-02-05 22:37:38 +0100

    feat(chooseCharacter): refactored chooseCharacterImpl #227

--- src/main/java/com/seinksansdoozebank/fr/model/player/RichardBot.java
+++ src/main/java/com/seinksansdoozebank/fr/model/player/RichardBot.java
@@ -96,6 +96,84 @@ public class RichardBot extends SmartBot {
         return (int) opponents.stream().filter(player -> player.getHandSize() == 0).count();
     }
 
+    /**
+     * tells us if players with more gold than the bot exist
+     *
+     * @param opponents list of the opponents of the player
+     * @return a boolean
+     */
+    protected boolean numberOfPlayerWithMoreGold(List<Opponent> opponents) {
+        return opponents.stream().anyMatch(opponent -> this.getNbGold() < opponent.getNbGold());
+    }
+
+    /**
+     * La méthode check si le joueur a plus de 7 cartes dans la main et si les autres joueurs ont des mains vides et renvoie un booléen.;
+     * Dans ce cas, il choisit l'assassin.
+     * @return a boolean
+     */
+    protected boolean shouldChooseAssassin() {
+        return getHand().size() >= 7 && numberOfEmptyHands(this.getOpponents()) >= 1;
+    }
+
+    /**
+     * La méthode check si le joueur a une main vide
+     * Dans ce cas, il choisit le magicien.
+     * @return a boolean
+     */
+    protected boolean shouldChooseMagician() {
+        return this.getHand().isEmpty();
+    }
+
+    /**
+     * La méthode check si le nombre d'or du joueur est inferieur ou égele à 1.
+     * Dans ce cas, il choisit le marchand.
+     * @return a boolean
+     */
+    protected boolean shouldChooseMerchant() {
+        return this.getNbGold() <= 1;
+    }
+
+    /**
+     * La méthode check si le joueur peut poser deux districts en plus dans sa citadelle et si ses opposants ont plus d'or que lui.
+     * Dans ce cas, il choisit l'architecte.
+     * @return a boolean
+     */
+    protected boolean shouldChooseArchitect() {
+        return getPriceOfNumbersOfCheaperCards(2) <= this.getNbGold() && numberOfPlayerWithMoreGold(this.getOpponents());
+    }
+
+
+    /**
+     * La méthode check si le joueur peut poser au moins un district
+     * Dans ce cas, il choisit l'évêque.
+     * @return a boolean
+     */
+    protected boolean shouldChooseBishop() {
+        return getPriceOfNumbersOfCheaperCards(1) <= this.getNbGold();
+    }
+
+    /**
+     * La méthode check si le joueur ne peut pas construire de district.
+     * Dans ce cas, il choisit le condottière.
+     * @return a boolean
+     */
+    protected boolean shouldChooseCondottiere() {
+        return getPriceOfNumbersOfCheaperCards(1) > this.getNbGold();
+    }
+
+    /**
+     * Cette méthode permet au joueur s'il est en position de poser son dernier district
+     * dans sa cité de choisir soit l'assassin, soit l'évêque, soit le condottière.
+     * @param characters list of characters available
+     * @return an optional of character
+     */
+    protected Optional<Character> shouldChooseBecauseLastCardToBuy(List<Character> characters) {
+        if (isAboutToWin()) {
+            return characters.stream().filter(c -> c.getRole() == Role.ASSASSIN || c.getRole() == Role.BISHOP || c.getRole() == Role.CONDOTTIERE).findFirst();
+        }
+        return Optional.empty();
+    }
+
 
 
 
