afa26a0f27a284ce007603e0ed1c3699dbced2bc
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-02-07 16:25:52 +0100

    refactor: changing some test that was duplicate into aparameterizedTest. #230

--- src/test/java/com/seinksansdoozebank/fr/controller/GameFactoryTest.java
+++ src/test/java/com/seinksansdoozebank/fr/controller/GameFactoryTest.java
@@ -7,6 +7,8 @@ import com.seinksansdoozebank.fr.model.player.custombot.CustomBot;
 import com.seinksansdoozebank.fr.view.IView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -51,26 +53,11 @@ class GameFactoryTest {
         assertThrows(IllegalArgumentException.class, () -> GameFactory.createGameOfSmartBot(view, 3));
     }
 
-    @Test
-    void createGameOfSmartBotWith4Bot() {
-        Game game = GameFactory.createGameOfSmartBot(view, 4);
-        assertEquals(4, game.players.size());
-        // Check the type of the players
-        game.players.forEach(player -> assertInstanceOf(SmartBot.class, player));
-    }
-
-    @Test
-    void createGameOfSmartBotWith5Bot() {
-        Game game = GameFactory.createGameOfSmartBot(view, 5);
-        assertEquals(5, game.players.size());
-        // Check the type of the players
-        game.players.forEach(player -> assertInstanceOf(SmartBot.class, player));
-    }
-
-    @Test
-    void createGameOfSmartBotWith6Bot() {
-        Game game = GameFactory.createGameOfSmartBot(view, 6);
-        assertEquals(6, game.players.size());
+    @ParameterizedTest
+    @CsvSource({"4, 4", "5, 5", "6, 6"})
+    void createGameOfSmartBotWith4Bot(int nbPlayers, int expected) {
+        Game game = GameFactory.createGameOfSmartBot(view, nbPlayers);
+        assertEquals(expected, game.players.size());
         // Check the type of the players
         game.players.forEach(player -> assertInstanceOf(SmartBot.class, player));
     }
@@ -85,26 +72,11 @@ class GameFactoryTest {
         assertThrows(IllegalArgumentException.class, () -> GameFactory.createGameOfCustomBot(view, 3));
     }
 
-    @Test
-    void createGameOfCustomBotWith4Bot() {
-        Game game = GameFactory.createGameOfCustomBot(view, 4);
-        assertEquals(4, game.players.size());
-        // Check the type of the players
-        game.players.forEach(player -> assertInstanceOf(CustomBot.class, player));
-    }
-
-    @Test
-    void createGameOfCustomBotWith5Bot() {
-        Game game = GameFactory.createGameOfCustomBot(view, 5);
-        assertEquals(5, game.players.size());
-        // Check the type of the players
-        game.players.forEach(player -> assertInstanceOf(CustomBot.class, player));
-    }
-
-    @Test
-    void createGameOfCustomBotWith6Bot() {
-        Game game = GameFactory.createGameOfCustomBot(view, 6);
-        assertEquals(6, game.players.size());
+    @ParameterizedTest
+    @CsvSource({"4, 4", "5, 5", "6, 6"})
+    void createGameOfCustomBotWith4Bot(int nbPlayers, int expected) {
+        Game game = GameFactory.createGameOfCustomBot(view, nbPlayers);
+        assertEquals(expected, game.players.size());
         // Check the type of the players
         game.players.forEach(player -> assertInstanceOf(CustomBot.class, player));
     }
@@ -119,26 +91,11 @@ class GameFactoryTest {
         assertThrows(IllegalArgumentException.class, () -> GameFactory.createGameOfRichardBot(view, 3));
     }
 
-    @Test
-    void createGameOfRichardBotWith4Bot() {
-        Game game = GameFactory.createGameOfRichardBot(view, 4);
-        assertEquals(4, game.players.size());
-        // Check the type of the players
-        game.players.forEach(player -> assertInstanceOf(RichardBot.class, player));
-    }
-
-    @Test
-    void createGameOfRichardBotWith5Bot() {
-        Game game = GameFactory.createGameOfRichardBot(view, 5);
-        assertEquals(5, game.players.size());
-        // Check the type of the players
-        game.players.forEach(player -> assertInstanceOf(RichardBot.class, player));
-    }
-
-    @Test
-    void createGameOfRichardBotWith6Bot() {
-        Game game = GameFactory.createGameOfRichardBot(view, 6);
-        assertEquals(6, game.players.size());
+    @ParameterizedTest
+    @CsvSource({"4, 4", "5, 5", "6, 6"})
+    void createGameOfRichardBotWith4Bot(int nbPlayers, int expected) {
+        Game game = GameFactory.createGameOfRichardBot(view, nbPlayers);
+        assertEquals(expected, game.players.size());
         // Check the type of the players
         game.players.forEach(player -> assertInstanceOf(RichardBot.class, player));
     }
